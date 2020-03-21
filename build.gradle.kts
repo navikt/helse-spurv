6f784fe85cf8926493043be7da5fcc624d79d3eb
David Steinsland <david@steinsland.email>
2020-03-21 10:15:03 +0100

    oppdater rapids-and-rivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ val githubUser: String by project
 val githubPassword: String by project
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.a468ae5")
+    implementation("com.github.navikt:rapids-and-rivers:1.f1afe50")
 
     implementation("org.flywaydb:flyway-core:$flywayVersion")
     implementation("com.zaxxer:HikariCP:$hikariVersion")

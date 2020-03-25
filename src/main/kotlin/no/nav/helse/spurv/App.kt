5753686cd0e71c9d8839472e103bae202cf93d74
David Steinsland <david@steinsland.email>
2020-03-25 11:32:40 +0100

    lag rapport med cronjob hver dag kl 7; øker replicas på db-innlesing

--- src/main/kotlin/no/nav/helse/spurv/App.kt
+++ src/main/kotlin/no/nav/helse/spurv/App.kt
@@ -2,24 +2,21 @@ package no.nav.helse.spurv
 
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
+import org.slf4j.LoggerFactory
+import java.time.LocalDate
 
 fun main() {
     val env = System.getenv()
+    if ("true" == env["CRON_JOB_MODE"]?.toLowerCase()) return rapportJob(env)
+    rapidApp(env)
+}
 
+private fun rapidApp(env: Map<String, String>) {
     val dataSourceBuilder = DataSourceBuilder(env)
     val dataSource = dataSourceBuilder.getDataSource()
 
-    val slackClient = env["SLACK_WEBHOOK_URL"]?.let {
-        SlackClient(
-            webhookUrl = it,
-            defaultChannel = "#område-helse-rapportering",
-            defaultUsername = "spurv"
-        )
-    }
-
     RapidApplication.create(env).apply {
         Tilstandrapportering(this, VedtaksperioderapportDao(dataSource), AktivitetsloggerAktivitetDao(dataSource))
-        Tilstandrapport(this, VedtaksperioderapportDao(dataSource), AktivitetsloggerAktivitetDao(dataSource), slackClient)
     }.apply {
         register(object : RapidsConnection.StatusListener {
             override fun onStartup(rapidsConnection: RapidsConnection) {
@@ -27,4 +24,19 @@ fun main() {
             }
         })
     }.start()
+}
+
+private fun rapportJob(env: Map<String, String>) {
+    Thread.setDefaultUncaughtExceptionHandler { _, throwable -> LoggerFactory.getLogger("no.nav.helse.Spurv").error(throwable.message, throwable) }
+    val dataSourceBuilder = DataSourceBuilder(env)
+    val dataSource = dataSourceBuilder.getDataSource(DataSourceBuilder.Role.ReadOnly)
+
+    val slackClient = SlackClient(
+        webhookUrl = env.getValue("SLACK_WEBHOOK_URL"),
+        defaultChannel = "#område-helse-rapportering",
+        defaultUsername = "spurv"
+    )
+
+    Tilstandrapport(VedtaksperioderapportDao(dataSource), AktivitetsloggerAktivitetDao(dataSource), slackClient)
+        .lagRapport(LocalDate.now().minusDays(1))
 }
